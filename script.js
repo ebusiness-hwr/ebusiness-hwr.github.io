5c3efdd1c1ad9fb45f900c38c2953f7d4c99ccfc
Niklas <60843393+niklaswendt@users.noreply.github.com>
2022-03-01 11:00:35 +0100

    Update 2.0
    frisch vom Tisch

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ let weather = {
     "apiKey": "d62f2715fe700226fb8682e966b851fa",
     fetchWeather: function (city) {
         fetch(
+            //API cal via city name
             "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city 
             + "&units=metric&appid=" 
             + this.apiKey
-        ).then((response) => response.json())
-        .then((data) => this.displayWeather(data));
+            ).then((response) => response.json())
+        .then((dataWeather) => this.displayWeather(dataWeather));
     },
-    displayWeather: function(data){
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } =data.wind;
+    displayWeather: function(dataWeather){
+        const { name } = dataWeather;
+        const { icon, description } = dataWeather.weather[0];
+        const { temp, humidity } = dataWeather.main;
+        const { speed } =dataWeather.wind;
         //console.log(name,icon,description,temp,humidity,speed);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/"+ icon +".png";
@@ -22,6 +23,8 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1600x900/?" + name+ "')";
+        document.querySelector(".regionalnews").innerText = "News of "+ name;
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
@@ -32,15 +35,51 @@ document
     .querySelector(".search button")
     .addEventListener("click", function() {
         weather.search();
+        news.search();
     }
     
     );
 
 document.querySelector(".search-bar").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
-        weather.search()
+        weather.search();
+        news.search();
     }
 }
 )
 
-//weather.fetchWeather("Berlin");
+let news={  
+    "ApiKey": "p1sP8KXdkcmUx8KdRaBbiKoKsWfOi80uCVNypCwM",
+    fetchNews: function (city){
+        var requestOptions = {
+            method: 'GET'
+        }
+        fetch(
+            "https://api.thenewsapi.com/v1/news/all?api_token="
+            + this.ApiKey
+            +"&search="
+            + city, requestOptions
+        ).then((response) => response.json())
+        .then((data) => this.displayNews(data));
+    },
+    displayNews: function(data){
+        console.log(data.data[0]);
+        const {title} = data.data[0];
+        const {description} = data.data[0];
+        const {source} = data.data[0];
+        const {url} = data.data[0];
+        document.querySelector(".title1").innerText = title;
+        document.querySelector(".description1").innerText = "\n"+ description;
+        document.querySelector(".source1").innerText = "\n Published by: "+ url;
+        //document.querySelector(".newsfeed2").innerText = 
+        document.querySelector(".news").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchNews(document.querySelector(".search-bar").value);
+    }
+};
+
+
+//zeigt am Anfang das Wetter von Berlin
+weather.fetchWeather("Berlin");
+news.fetchNews("Berlin");
